f7266f16952e0652ec383104365f963247195129
brentmc <brentmcivor.mail+workgithub@gmail.com>
2015-06-25 12:53:21 +1000

    2 - Created Container.js

---
+++ app/Container.js
@@ -0,0 +1,47 @@
+function Container() {
+    this.renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0x1099bb});
+    document.body.appendChild(this.renderer.view);
+
+// create the root of the scene graph
+    this.stage = new PIXI.Container();
+
+    var container = new PIXI.Container();
+
+    this.stage.addChild(container);
+
+    for (var j = 0; j < 5; j++) {
+
+        for (var i = 0; i < 5; i++) {
+            var bunny = PIXI.Sprite.fromImage('_assets/basics/penpen.png');
+            bunny.x = 40 * i;
+            bunny.y = 40 * j;
+            container.addChild(bunny);
+        };
+    };
+    /*
+     * All the bunnies are added to the container with the addChild method
+     * when you do this, all the bunnies become children of the container, and when a container moves,
+     * so do all its children.
+     * This gives you a lot of flexibility and makes it easier to position elements on the screen
+     */
+    container.x = 100;
+    container.y = 60;
+
+    var self = this;
+    window.requestAnimationFrame(function () {
+        self.animate()
+    });
+}
+
+
+Container.prototype.animate = function() {
+
+    var self = this;
+    //requestAnimationFrame(self.animate);
+    window.requestAnimationFrame(function () {
+        self.animate()
+    });
+
+    // render the container
+    this.renderer.render(self.stage);
+};
